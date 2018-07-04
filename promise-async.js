d8357570ba7c220315e6937764586afcd94da7ba
Tilo Dietrich <tilo.dietrich@mynewsdesk.com>
2018-07-04 11:35:39 +0200

    Initial commit

---
+++ promise-async.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const promiseFn = (number) => {
+  return new Promise((resolve, reject) => {
+    if (number > 0) {
+      resolve(number * 2);
+    } else {
+      reject('Nope');
+    }
+  });
+};
+
+const asyncFn = async (number) => {
+  if (number > 0) {
+    return number * 2;
+  } else {
+    throw 'Nope';
+  }
+}
+
+promiseFn(2).then(console.log).catch(console.log);
+promiseFn(-1).then(console.log).catch(console.log);
+asyncFn(2).then(console.log).catch(console.log);
+asyncFn(-1).then(console.log).catch(console.log);
