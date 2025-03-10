a3412711cf92ccfc89d3295120e6918f0cee204e
Tilo Dietrich <git@tilod.dev>
2025-03-10 10:27:43 +0100

    Improve class vs. closure

--- class-vs-closure.js
+++ class-vs-closure.js
@@ -3,11 +3,17 @@
 /*
  * Classes and closures are the same!
  * ==================================
+ *
+ * ... so you can mimic object-oriented programming in functional languages.
  */
 
 /*
  + Member variables for classes are used to store state during the lifetime of
  * an instance of the class.
+ *
+ *   - The constructor sets the initial state of the instance. The state is
+ *     stored as long as the instance exists.
+ *   - Methods access the state of the instance.
  */
 class MyClass {
   constructor(value) {
@@ -15,31 +21,52 @@ class MyClass {
     this.fixedValue = 2;
   }
 
-  printValue() {
-    console.log(this.passedValue * this.fixedValue);
+  add() {
+    return this.passedValue + this.fixedValue;
+  }
+
+  multiply() {
+    return this.passedValue * this.fixedValue;
   }
 }
 
 /*
- * Closures are functions which
+ * Closures are functions that have access to the outer scope in which they were
+ * created, even after that scope has closed. This allows them to store state
+ * between calls.
+ *
+ *   - The outer scope is the function in which the closures are created. It
+ *     acts as a constructor and sets the initial state of the closures. The
+ *     state is stored as local variables in the outer scope, but they exist as
+ *     long as the _closures_ (not the outer scope) exist.
+ *   - The outer scope returns an object with two functions defined inside it.
+ *     These functions are the closures.
  */
-function MyClosure(value) {
+function MyClosureScope(value) {
   const passedValue = value;
   const fixedValue = 2;
 
-  const printValue = () => {
-    console.log(passedValue * fixedValue);
+  const add = function() {
+    return passedValue + fixedValue;
+  }
+
+  const multiply = function() {
+    return passedValue * fixedValue;
   };
 
-  return { printValue };
+  return { add, multiply };
 }
 
 /*
  * You can use both the same way
  */
 const myClassInstance = new MyClass(3);
-const myClosureInstance = MyClosure(3);
-myClassInstance.printValue();
-myClosureInstance.printValue();
+const myClosureInstance = MyClosureScope(3);
+console.log(myClassInstance.add());
+console.log(myClassInstance.multiply());
+console.log(myClosureInstance.add());
+console.log(myClosureInstance.multiply());
+// => 5
 // => 6
+// => 5
 // => 6
